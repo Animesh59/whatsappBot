0a3790bc09423a404ba0a789ba7eff3ed4d6136e
Animesh59 <abelieve0304@gmail.com>
2024-05-12 15:17:54 +0530

    Reconfigured the structured

--- index.js
+++ index.js
@@ -1,103 +1,21 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+//--------------------- Express routes for REST-APIs ---------------------
+const whatsapp = require("./whatsappLogic/logic")
+const express = require("express");
+const app = express();
+const PORT = 8080;
 
-
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    webVersion: '2.2409.2',
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.2.html'
-    }
-});
-
-// console.log(client);
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-
-    // ------------------------ sendMessage ------------------------
-    // const number = "+919732531193";
-    // const text = "Testing my whatsapp bot\n type---> \n hi  or  ping  or kirtan";
-    // const chatId = number.substring(1) + "@c.us";
-    // client.sendMessage(chatId, text)
-    //     .then(() => console.log(`Successfully message sent to ${number}`))
-    //     .catch((err) => console.log(`Failed to send message\n${err}`));
-
-    //Sending 
-    // client.sendMessage("918436627009@c.us", 'Guitar message instantly when the client is ready')
-    //     .then(() => console.log(`Successfully message sent message`))
-    //     .catch((err) => console.log(`Failed to send message\n${err}`));
-
-    //Checking wheather the whatspp user exitst
-    // client.isRegisteredUser("919732531193@c.us")
-    //     .then((res) => console.log(`------------------------------------------Here goes Baba's the result----------------------------------\n${res}`))
-    //     .catch((rej) => console.log(`------------------------------------------Here goes Baba's the rejection----------------------------------\n${rej}`));
-
-    // client.isRegisteredUser("919474318868@c.us")
-    //     .then((res) => console.log(`------------------------------------------Here goes Provu's the result----------------------------------\n${res}`))
-    //     .catch((rej) => console.log(`------------------------------------------Here goes Provu's the rejection----------------------------------\n${rej}`));
-
-    // client.getContacts()
-    //     .then((data) => console.log(data))
-    //     .catch((err) => console.log(err));
-});
-
-client.on("authenticated", () => {
-    console.log('Successfully Authenticated!');
+app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.originalUrl}`);
+    next();
 })
 
-client.on("disconnected", () => {
-    console.log('client is disconnected');
+app.get("/", (req, res) => {
+    res.send(`<h1>Welcome to root page</h1>`);
+});
+
+app.post("/send-whatsapp-message", express.json(), async (req, res) => {
+    await whatsapp.sendMessage(req.body.contact, req.body.message);
+    res.send('successfully sent messages');
 })
 
-client.on('qr', qr => {
-    console.log(qr);
-});
-
-
-// Listening to all incoming messages
-client.on('message_create', async (message) => {
-    // console.log(message);
-    console.log(`${message.from}-${message._data.notifyName}: ${message.body}`);
-
-    m = message.body.toLowerCase();
-
-    if (m === 'ping') {
-        // message.reply('pong');
-        client.sendMessage(message.from, 'pong');
-        return 0;
-    }
-
-    if (m === 'hi') {
-        // message.reply(`Hello I am Animesh's bot`);
-        // message.reply(`How can I help you?`);
-        client.sendMessage(message.from, `Hello I am Animesh's bot`);
-        client.sendMessage(message.from, `How can I help you?`);
-        return 0;
-    }
-
-    if (m === 'kirtan') {
-        // message.reply('Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Ram Hare Ram Ram Ram Hare Hare \n https://www.youtube.com/watch?v=r6WBtDnLxwM')
-        client.sendMessage(message.from, 'Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Ram Hare Ram Ram Ram Hare Hare \n https://www.youtube.com/watch?v=r6WBtDnLxwM');
-        return 0;
-    }
-
-    if (m === '🚗') {
-        client.sendMessage(message.from, `Lets ride on this red car & go to Mayapur dham`);
-        return 0;
-    }
-
-});
-
-
-client.initialize();
-
-console.log('Please wait initializing...');
-
-//Closing correcily using CTRL+C 
-process.on('SIGINT', async () => {
-    console.log('(SIGINT) Shutting down...');
-    await client.destroy();
-    console.log('client destroyed');
-    process.exit(0);
-});
+app.listen(PORT, () => console.log(`server is running on: http://localhost:${PORT}`));
--- whatsappLogic/logic.js
+++ whatsappLogic/logic.js
@@ -1,5 +1,6 @@
 //--------------------- whatsapp-web.js packages ---------------------
 const { Client, LocalAuth } = require("whatsapp-web.js");
+
 const whatsapp = new Client({
     authStrategy: new LocalAuth(),
     webVersion: '2.2409.2',
@@ -8,6 +9,9 @@ const whatsapp = new Client({
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.2.html'
     }
 })
+whatsapp.on('qr', qr => {
+    console.log(qr);
+});
 
 whatsapp.on("authenticated", () => {
     console.log('Your whatsapp account is authenticated');
@@ -46,6 +50,7 @@ whatsapp.on("message_create", async (message) => {
 });
 
 whatsapp.initialize();
+console.log('Please wait initializing...');
 
 process.on('SIGINT', async () => {
     console.log('(SIGINT) Shutting down...');
@@ -54,28 +59,4 @@ process.on('SIGINT', async () => {
     process.exit(0);
 });
 
-//--------------------- Express routes for REST-APIs ---------------------
-const express = require("express");
-const app = express();
-const PORT = 8080;
-
-app.use((req, res, next) => {
-    console.log(`${req.method}: ${req.originalUrl}`);
-    next();
-})
-
-app.get("/", (req, res) => {
-    res.send(`<h1>Welcome to root page</h1>`);
-});
-
-app.post("/send-whatsapp-message", express.json(), async (req, res) => {
-    // var contact = '918436627009@c.us';
-    // var message = 'Hello I am Animesh Believe';
-    // console.log(req.body);
-    await whatsapp.sendMessage(req.body.contact, req.body.message);
-    res.send('successfully sent messages');
-})
-
-app.listen(PORT, () => console.log(`server is running on: http://localhost:${PORT}`));
-
-console.log('Please wait initializing...');
+module.exports = whatsapp;
